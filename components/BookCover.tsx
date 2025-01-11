4ca66aa4ffa5b228160b1d7c2228632b9966ff07
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-11 14:22:37 +0530

    Added Home page and auth page and layout

---
+++ components/BookCover.tsx
@@ -0,0 +1,36 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import React from 'react'
+import BookCoverSvg from './BookCoverSvg'
+type BookCovervarient = "extraSmall" | "small" | "medium" | "regular" |"wide"
+const varientStyles :Record<BookCovervarient , string> = {
+    extraSmall :'book-cover_extra_small',
+    small :'book-cover_small',
+    medium :'book-cover_medium',
+    regular :'book-cover_regular',
+    wide :'book-cover_wide',
+}
+interface Props{
+    className?:string
+varient?:BookCovervarient
+coverColor:string
+coverUrl:string
+}
+const BookCover = ({className , varient='regular' , coverColor='#012B48' , coverUrl ="https://placehold.co/400x600.png"}:Props) => {
+  return (
+    <div className={cn('relative transition-all duration-300' , varientStyles[varient] , className)}>
+       <BookCoverSvg coverColor={coverColor}/>
+        <div className='absolute z-10' style={{left:'12%' , width:"87.5%" , height:"88"}}>
+            <Image
+            src={coverUrl}
+            alt='book cover'
+            width={300}
+            height={300}
+            className='rounded-sm object-cover'
+            />
+        </div>
+    </div>
+  )
+}
+
+export default BookCover
