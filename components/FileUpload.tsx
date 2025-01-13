ea20bae4f99a23c0da8e221827df9d9a2db62845
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-13 20:34:39 +0530

    Solved some error

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -130,13 +130,13 @@ const onValidate  =(file : File)=>{
     )}
     {file && (
         (type === 'image' ? (  <IKImage
-        alt={file.filePath}
-        path={file.filePath}
+        alt={file.filePath!}
+        path={file.filePath!}
         width={500}
         height={300}
 />): type === 'video' ?(
 <IKVideo
-path={file.filePath}
+path={file.filePath!}
 controls={true}
 className='h-96 w-full '
 />
